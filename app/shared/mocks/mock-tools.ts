321f3f5d78c02ac376b66bec03a14b348d0cc000
Kevin Liozon <kevinliozonpro@gmail.com>
2017-05-06 13:32:47 +0100

    Tools
    Modified Tools’ model. Added all Tools’ data. Bound them to about
    component.

--- app/shared/mocks/mock-tools.ts
+++ app/shared/mocks/mock-tools.ts
@@ -2,20 +2,137 @@ import { Tool } from '../models/tool';
 
 
 export let TOOLS: Tool[] = [{
-  "id": "#tool-1",
-  "title": "Atom",
-  "desc": "blabla",
-  "image": "string",
-  "alt": "string",
-  "tag": "Front, UX, Symfony, Foundation, Angular.js",
-  "view": "string",
+  "id": "#tool15",
+  "title": "VirtualBox",
+  "featured": true,
+  "category": "Virtualization tool",
+  "desc": "Tool for taking notes",
+  "image": "../../../assets/img/svg/tools/virtualbox.svg",
+  "view": "https://en.wikipedia.org/wiki/VirtualBox"
 },
 {
-  "id": "#tool-2",
+  "id": "#tool14",
+  "title": "Filezilla",
+  "featured": true,
+  "category": "FTP Client",
+  "desc": "Classic tool for deployment on server",
+  "image": "../../../assets/img/svg/tools/filezilla.svg",
+  "view": "https://en.wikipedia.org/wiki/FileZilla"
+},
+{
+  "id": "#tool13",
+  "title": "Evernote",
+  "featured": false,
+  "category": "Project Management",
+  "desc": "Tool for taking notes",
+  "image": "../../../assets/img/svg/tools/evernote.svg",
+  "view": "https://en.wikipedia.org/wiki/Evernote"
+},
+{
+  "id": "#tool12",
+  "title": "Toggl",
+  "featured": true,
+  "category": "Project Management",
+  "desc": "Project timer tool",
+  "image": "../../../assets/img/svg/tools/toggl.svg",
+  "view": "https://en.wikipedia.org/wiki/Toggl"
+},
+{
+  "id": "#tool11",
+  "title": "Homebrew",
+  "featured": false,
+  "category": "Package Manager",
+  "desc": "Package manager for OSX platforms",
+  "image": "../../../assets/img/svg/tools/homebrew.svg",
+  "view": "https://en.wikipedia.org/wiki/Homebrew_(package_management_software)"
+},
+{
+  "id": "#tool10",
+  "title": "NPM",
+  "featured": true,
+  "category": "Package Manager",
+  "desc": "Default package manager for every project",
+  "image": "../../../assets/img/svg/tools/npm.svg",
+  "view": "https://en.wikipedia.org/wiki/Npm_(software)"
+},
+{
+  "id": "#tool9",
+  "title": "Microsoft Office",
+  "featured": false,
+  "category": "Collaborative",
+  "desc": "Cloud service & Desktop Softwares",
+  "image": "../../../assets/img/svg/tools/office.svg",
+  "view": "https://en.wikipedia.org/wiki/Microsoft_Office"
+},
+{
+  "id": "#tool8",
+  "title": "Slack",
+  "featured": true,
+  "category": "Collaborative",
+  "desc": "Cloud service",
+  "image": "../../../assets/img/svg/tools/slack.svg",
+  "view": "https://en.wikipedia.org/wiki/Slack_Technologies"
+},
+{
+  "id": "#tool7",
+  "title": "Trello",
+  "featured": true,
+  "category": "Project Management",
+  "desc": "Kanban based project management tool",
+  "image": "../../../assets/img/svg/tools/trello.svg",
+  "view": "https://en.wikipedia.org/wiki/Trello"
+},
+{
+  "id": "#tool6",
+  "title": "Google Drive",
+  "featured": false,
+  "category": "Collaborative",
+  "desc": "Cloud service",
+  "image": "../../../assets/img/svg/tools/googledrive.svg",
+  "view": "https://en.wikipedia.org/wiki/Google_Drive"
+},
+{
+  "id": "#tool5",
+  "title": "Atlassian",
+  "featured": true,
+  "category": "Collaborative",
+  "desc": "Suite of tools: BitBucket, Jira, Sourcetree and so on",
+  "image": "../../../assets/img/svg/tools/atlassian.svg",
+  "view": "https://en.wikipedia.org/wiki/Atlassian"
+},
+{
+  "id": "#tool4",
+  "title": "GitHub",
+  "featured": true,
+  "category": "Collaborative",
+  "desc": "Web & Destktop Git UI tool",
+  "image": "../../../assets/img/svg/tools/github.svg",
+  "view": "https://en.wikipedia.org/wiki/GitHub"
+},
+{
+  "id": "#tool3",
+  "title": "xCode",
+  "featured": true,
+  "category": "IDE",
+  "desc": "Mandatory IDE for iOS development",
+  "image": "../../../assets/img/svg/tools/xcode.svg",
+  "view": "https://en.wikipedia.org/wiki/Xcode"
+},
+{
+  "id": "#tool2",
   "title": "PHP Storm",
-  "desc": "blabla",
-  "image": "string",
-  "alt": "string",
-  "tag": "Front, UX, Symfony, Foundation, Angular.js",
-  "view": "string",
+  "featured": false,
+  "category": "IDE",
+  "desc": "Other IDE quite useful for Symfony projects",
+  "image": "../../../assets/img/svg/tools/phpstorm.svg",
+  "view": "https://en.wikipedia.org/wiki/PhpStorm"
+},
+{
+  "id": "#tool1",
+  "title": "Atom",
+  "featured": true,
+  "category": "IDE",
+  "desc": "Powerful, flexible and free IDE developed by GitHub",
+  "image": "../../../assets/img/svg/tools/atom.svg",
+  "view": "https://en.wikipedia.org/wiki/Atom_(text_editor)"
 }];
