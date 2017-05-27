4ed375bd7a56b0976b006bb9b9e8a7e060a07fa5
Kevin Liozon <kevinliozonpro@gmail.com>
2017-05-27 19:33:43 +0100

    Diploma model and contents.

--- app/shared/mocks/mock-diplomas.ts
+++ app/shared/mocks/mock-diplomas.ts
@@ -2,36 +2,140 @@ import { Diploma } from '../models/diploma';
 
 
 export let DIPLOMAS: Diploma[] = [{
-  "id": "#diploma-1",
-  "featured": true,
-  "degree": "Baccalaureate",
-  "field": "Science, speciality biology, option Physics/chemistry",
-  "school": "Louis Feuillade Highschool",
-  "place": "Lunel - France",
-  "grade": "10/20",
-  "desc": "blabla",
-  "image": "string",
-  "alt": "string",
-  "date_start": "2016-12-01",
-  "date_end": "2016-12-01",
-  "tag": "Front, UX, Symfony, Foundation, Angular.js",
-  "view": "string",
-  "proof": "string"
+  "id": "#diploma6",
+  "info": {
+    "type": "certification",
+    "field": "UX certification",
+    "thumb": "../../../assets/img/thumb/diplomas/nng.png",
+    "image": "../../../assets/img/png/diplomas/nng.png",
+    "subject1": "Interaction Design (speciality 1)",
+    "subject2": "Webdesign (speciality 2)",
+    "subject3": "UX management"
+  },
+  "school": {
+    "name": "Nielsen & Norman Group",
+    "image": "../../../assets/img/svg/education/nng.svg"
+  },
+  "dates": {
+    "start": "2016-08-15",
+    "end": "2016-11-14"
+  },
+  "link": "https://www.codeschool.com/"
 },
 {
-  "id": "#diploma-2",
-  "featured": true,
-  "degree": "University Degree (DU)",
-  "field": "Webmaster",
-  "school": "Institute of Technology",
-  "place": "Montpellier - France",
-  "grade": "10/20",
-  "desc": "A social sharing platform for 3D designers",
-  "image": "string",
-  "alt": "string",
-  "date_start": "2016-12-01",
-  "date_end": "2016-12-01",
-  "tag": "Front, UX, Symfony, Foundation, Angular.js",
-  "view": "string",
-  "proof": "string"
+  "id": "#diploma5",
+  "info": {
+    "type": "certification",
+    "field": "Dev certifications",
+    "thumb": "../../../assets/img/svg/education/codeschool.svg",
+    "image": "../../../assets/img/svg/education/codeschool.svg",
+    "subject1": "Javascript",
+    "subject2": "Angular",
+    "subject3": "Sass"
+  },
+  "school": {
+    "name": "CodeSchool",
+    "image": "../../../assets/img/svg/education/codeschool.svg"
+  },
+  "dates": {
+    "start": "2016-03-01",
+    "end": "2017-01-01"
+  },
+  "link": "https://www.codeschool.com/"
+},
+{
+  "id": "#diploma4",
+  "info": {
+    "type": "degree",
+    "class": "Bachelor's Degree (Licence Pro)",
+    "field": "Webdesign",
+    "thumb": "../../../assets/img/thumb/diplomas/lp.png",
+    "image": "../../../assets/img/png/diplomas/lp.png",
+    "subject1": "Web Dev",
+    "subject2": "UX Design",
+    "subject3": "Project Management"
+  },
+  "school": {
+    "name": "University of Burgundy - Franche-Comté",
+    "place": "Montbéliard",
+    "country": "France",
+    "image": "../../../assets/img/svg/education/ubfc.svg"
+  },
+  "dates": {
+    "start": "2015-09-01",
+    "end": "2016-06-20"
+  },
+  "link": "https://en.wikipedia.org/wiki/University_of_Burgundy_-_Franche-Comt%C3%A9"
+},
+{
+  "id": "#diploma3",
+  "info": {
+    "type": "degree",
+    "class": "Two years university degree in technology (DUT)",
+    "field": "Web & Multimedia Technologies (MMI)",
+    "thumb": "../../../assets/img/thumb/diplomas/mmi.png",
+    "image": "../../../assets/img/png/diplomas/mmi.png",
+    "subject1": "Web Dev (speciality)",
+    "subject2": "Visual Design",
+    "subject3": "Information System"
+  },
+  "school": {
+    "name": "Institute of Technology",
+    "place": "Béziers",
+    "country": "France",
+    "image": "../../../assets/img/svg/education/um.svg"
+  },
+  "dates": {
+    "start": "2013-09-01",
+    "end": "2015-06-01"
+  },
+  "link": "https://en.wikipedia.org/wiki/University_of_Montpellier"
+},
+{
+  "id": "#diploma2",
+  "info": {
+    "type": "degree",
+    "class": "University Degree (DU)",
+    "field": "Webmaster",
+    "thumb": "../../../assets/img/thumb/diplomas/du.png",
+    "image": "../../../assets/img/png/diplomas/du.png",
+    "subject1": "Web Dev",
+    "subject2": "Mathematics",
+    "subject3": "Visual Design"
+  },
+  "school": {
+    "name": "Institute of Technology",
+    "place": "Montpellier",
+    "country": "France",
+    "image": "../../../assets/img/svg/education/um.svg"
+  },
+  "dates": {
+    "start": "2012-09-01",
+    "end": "2013-06-01"
+  },
+  "link": "https://en.wikipedia.org/wiki/University_of_Montpellier"
+},
+{
+  "id": "#diploma1",
+  "info": {
+    "type": "degree",
+    "class": "Baccalaureate",
+    "field": "Science, speciality biology, option Physics/chemistry",
+    "thumb": "../../../assets/img/thumb/diplomas/bac.png",
+    "image": "../../../assets/img/png/diplomas/bac.png",
+    "subject1": "Biology (speciality)",
+    "subject2": "Physics/chemistry (option)",
+    "subject3": "Mathematics"
+  },
+  "school": {
+    "name": "Louis Feuillade Highschool",
+    "place": "Lunel",
+    "country": "France",
+    "image": "../../../assets/img/svg/education/lf.svg"
+  },
+  "dates": {
+    "start": "2007-09-01",
+    "end": "2010-07-01"
+  },
+  "link": "http://www.louis-feuillade.com/"
 }];
