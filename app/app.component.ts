509be207403a6474eb918a14b6a6a2b437552baa
Kevin Liozon <kevinliozonpro@gmail.com>
2018-05-17 08:58:47 +0100

    scroll top animation for every page change across the app. shorter ellipsis for preview desc

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from "@angular/router";
 
 @Component({
   moduleId: module.id.replace("/dist/app/", "/app/"),
   selector: 'my-app',
   templateUrl: 'app.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  constructor() {  }
+  constructor(private router: Router) {  }
+  
+  ngOnInit() {
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+        return;
+      }// animation trigger
+      document.getElementById("anchor-top").scrollIntoView({block: "start", behavior: "smooth"});
+    });
+  }
 
 }
